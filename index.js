db699df050b831602471075ba304695720ef08ad
Michel Leermakers <michel.leermakers@tuni.fi>
2023-03-19 23:11:32 +0200

    Additions to puhelinluettelo backend

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(express.json())
 app.use(cors())
@@ -19,53 +35,30 @@ app.use(
   )
 )
 
-let phonebook = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122',
-  },
-]
-
-const generateId = () => {
-  return Math.floor(Math.random() * 10000)
-}
-
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook)
+  Person.find({}).then((people) => {
+    res.json(people)
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const info = phonebook.find((info) => info.id === id)
-
-  if (info) {
-    res.json(info)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  phonebook = phonebook.filter((info) => info.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -75,30 +68,43 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({ error: 'content missing' })
   }
 
-  if (phonebook.find((info) => info.name === body.name)) {
-    return res.status(400).json({ error: 'name exists already' })
-  }
-
-  const newInfo = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
+  })
+
+  person.save().then((savedPerson) => {
+    res.json(savedPerson)
+  })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
   }
 
-  phonebook = phonebook.concat(newInfo)
-
-  res.json(newInfo)
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.get('/info', (req, res) => {
-  res.send(
-    `<div><p>Phonebook has info for ${
-      phonebook.length
-    } people</p><p>${new Date()}</p></div>`
-  )
+  Person.countDocuments({}).then((result) => {
+    res.send(
+      `<div><p>Phonebook has info for ${result} people</p><p>${new Date()}</p></div>`
+    )
+  })
 })
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
